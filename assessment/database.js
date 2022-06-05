7fb757bbd5e572e00bb46367aa532e05a0365d35
Jett Thompson <jett.thompson.59@gmail.com>
2022-06-05 12:11:22 +1000

    created indexes for making the lookups in mongodb much faster

--- assessment/database.js
+++ assessment/database.js
@@ -6,6 +6,11 @@ const db = mongoConnection.then(client => client.db('notes-database'));
 const loginsCollection = db.then(db => db.collection('logins'));
 const notesCollection = db.then(db => db.collection('notes'));
 
+// Since creating the indexes multiple times doesn't hurt we just create them every time we connect to the database.
+// The only queries so far are using the username.
+loginsCollection.then(logins => logins.createIndex({ "username": 1 }));
+notesCollection.then(notes => notes.createIndex({ "username": 1 }));
+
 function getLogin(username) {
     return loginsCollection.then(logins => logins.findOne({ "username": username }));
 }
