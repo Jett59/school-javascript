3cb6ca38a146310cb7a4692d9f6d16c43978a6c6
Jett Thompson <jett.thompson.59@gmail.com>
2022-06-17 12:37:28 +1000

    made the code use a throttle instead of a debounce so that the text is saved periodicly while the user is typing

--- assessment/content/helper.js
+++ assessment/content/helper.js
@@ -2,14 +2,14 @@ function apiFetch(url, callback, options={method: "GET"}) {
     fetch(url, options).then(response => response.json()).then(callback);
 }
 
-function debounce(delay = 250) {
-    var timeout; // Allow us to cancel previous timeouts
-    return (func) => {
-        if (timeout) {
-            clearTimeout(timeout);
+function throttle(interval, callback) {
+    var timeoutId = undefined;
+    return () => {
+        if (timeoutId === undefined) {
+            timeoutId = setTimeout(() => {
+                callback();
+                timeoutId = undefined;
+            }, interval);
         }
-        timeout = setTimeout(() => {
-            func();
-        }, delay);
     };
 }
